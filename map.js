895df9ea32e3ee463b358d7e84bee553c4e6bfa3
aestefam <47981990+aestefam@users.noreply.github.com>
2019-03-01 11:29:55 -0500

    Update map.js

--- map.js
+++ map.js
@@ -102,4 +102,4 @@ let popup4 = new mapboxgl.Popup()
 popup4.setHTML('This is my Alma Mater (2006 to 2011) <br /><img src="https://i.ytimg.com/vi/8CYPOiJp2_I/maxresdefault.jpg" />')
 marker4.setPopup(popup4)
 
-})
+
